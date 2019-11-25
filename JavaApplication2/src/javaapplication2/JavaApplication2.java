40d47b40349bccdde8165669aa4c0bcd377f5de3
MarielleMagno <57879929+MarielleMagno@users.noreply.github.com>
2019-11-25 09:47:58 +0800

    Add files via upload

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+       for (int i=4; i >=0; i--){
+         for (int j =1; j>= i; j++){
+             System.out.print("* ");
+         }
+         System.out.println();
+     }  
+     for (int i = 1; i <= 6; i++){
+         for (int j =1 ; j <= i; ++j){
+             if(i%2==0){
+             System.out.print("* ");
+     }
+    System.out.println();
+    }
+    }
+    
+}
+}
